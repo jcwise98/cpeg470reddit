4c8c26b3a8985fae5a89187efdc2a5dd534dc763
jcwise98 <jcwise@udel.edu>
2019-11-17 00:26:59 -0500

    stuff

---
+++ fireBase.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBBj93-0ZUhkz31nT8u-lGD9982zP0Tf1Y",
+    authDomain: "cpeg470-reddit.firebaseapp.com",
+    databaseURL: "https://cpeg470-reddit.firebaseio.com",
+    projectId: "cpeg470-reddit",
+    storageBucket: "cpeg470-reddit.appspot.com",
+    messagingSenderId: "530492936109",
+    appId: "1:530492936109:web:cfeb7b01d2240ae48c1b24"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
